abfdfeb1c46612b116341f702961cd3cfad84c87
Amir Hamza <amirhamza771600@gmail.com>
2024-04-17 20:12:50 +0500

    app title fix

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,8 @@ import '../lib/fontawesome';
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "BOR WEB",
-  description: "BOR WEB",
+  title: "Book of Rhymes",
+  description: "Book of Rhymes",
 };
 
 export default function RootLayout({ children }) {
